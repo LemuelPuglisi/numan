bf1ae79c130b8ad0c840f5c8eae475947dff09dc
Charlemagne <lemuelpuglisi001@gmail.com>
2022-03-08 11:03:01 +0100

    feat: add function to develop

--- numan/errorstheory.py
+++ numan/errorstheory.py
@@ -1,2 +1,32 @@
 def name():
     print('errors theory')
+
+
+def absolute_error(real, mesure):
+    """ 
+    """
+    return None
+
+
+def relative_error(real, mesure):
+    """
+    """
+    return None
+
+
+def test_1(real, mesure, digits):
+    """
+    """
+    return None
+
+
+def test_2(real, mesure, digits):
+    """
+    """
+    return None
+
+
+def horner_scheme(coefficients, point):
+    """
+    """
+    return None
