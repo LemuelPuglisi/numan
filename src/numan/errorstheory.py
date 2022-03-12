1fad26cc199ff3b429417f5b925c76ab256e5698
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2022-03-12 12:04:34 +0100

    feat: add machine number set

--- src/numan/errorstheory.py
+++ src/numan/errorstheory.py
@@ -1,4 +1,8 @@
+from tkinter import E
 import numpy as np
+
+from enum import Enum
+
 
 def absolute_error(real, mesure):
     """ Mesure the goodness of the approximation
@@ -25,3 +29,59 @@ def p_correct_decimals(real, mesure, p):
 # Machine discretization functions 
 #
 
+class Approximations(Enum):
+    """ Enum containing the approximation methods. 
+    """
+    CHOPPING = 1
+    ROUNDING = 2
+
+
+class MachineNumberSet:
+    """ This class expose the characteristics of a machine number 
+        set, using the floating point mechanism. So, given a base b, 
+        the number of bits t assigned to the significand, a lower bound
+        L and and upper bound U to the exponent, we build the machine
+        number set.   
+    """
+
+    epsilon_fmap = { 
+        Approximations.CHOPPING: lambda b, t: b ** (- t + 1), 
+        Approximations.ROUNDING: lambda b, t: b ** (- t + 1) * .5 
+    }
+
+
+    def __init__(self, b, t, U, L, approx = Approximations.ROUNDING):
+        self.base   = b
+        self.bits   = t
+        self.lower  = L
+        self.upper  = U
+        self.approx = approx
+
+
+    def cardinality(self):
+        """ Since the machine number set is a discrete, finite set, 
+            we can calculate precisely its dimension as it follows. 
+        """
+        a = self.base ** (self.bits - 1)
+        r = self.upper - self.lower + 1
+        return 1 + 2 * (self.base - 1) * a * r
+
+
+    def smallest(self):
+        """ Returns the smallest number rapresentable.
+        """
+        return self.base ** (self.lower - 1)
+
+
+    def largest(self):
+        """ Returns the largest number rapresentable.
+        """
+        return (self.base ** self.upper) * (1 - self.base ** (-self.bits))
+
+
+    def machine_epsilon(self, approx=Approximations.ROUNDING):
+        """ Returns the machine epsilon
+        """
+        assert self.approx in Approximations
+        f = self.epsilon_fmap[approx]
+        return f(self.base, self.bits)
--- tests/test_errorstheory.py
+++ tests/test_errorstheory.py
@@ -1,5 +1,7 @@
+import numpy as np
 
 from numan import errorstheory as e
+
 
 def test_absolute_error():
     assert e.absolute_error(992, 1001) == 9
@@ -13,3 +15,34 @@ def test_p_correct_decimals():
     x = 624.428731
     y = 624.428711
     assert e.p_correct_decimals(x, y, 4)
+
+
+def test_machine_number_set_cardinality():
+    mns = e.MachineNumberSet(b=2, t=23, U=127, L=-127)
+    assert mns.cardinality() == 2139095041
+
+
+def test_machine_number_set_largest():
+    mns = e.MachineNumberSet(b=2, t=23, U=127, L=-127)
+    assert mns.largest() == 1.7014116317805963e+38
+
+
+def test_machine_number_set_smallest():
+    mns = e.MachineNumberSet(b=2, t=23, U=127, L=-127)
+    assert mns.smallest() == 2.938735877055719e-39
+
+
+def test_machine_epsilon_chopping(): 
+    mns = e.MachineNumberSet(b=2, t=23, U=127, L=-127, approx=e.Approximations.CHOPPING)
+    eps = mns.machine_epsilon()
+    exp = 19e-8
+    print(eps - exp)
+    assert np.abs(eps - exp) < 1e-7
+
+
+def test_machine_epsilon_rounding(): 
+    mns = e.MachineNumberSet(b=2, t=23, U=127, L=-127, approx=e.Approximations.ROUNDING)
+    eps = mns.machine_epsilon()
+    exp = 19e-8
+    print(eps - exp)
+    assert np.abs(eps - exp) < 1e-7
